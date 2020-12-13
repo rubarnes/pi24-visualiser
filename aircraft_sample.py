5abbca7861a064df88d4389a0946519b6665c11f
Ruairidh Barnes <ruairidhb@gmail.com>
2020-12-13 20:04:09 +0000

    initial commit

---
+++ aircraft_sample.py
@@ -0,0 +1,17 @@
+{ "now" : 1607878253.7,
+  "messages" : 355,
+  "aircraft" : [
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8160 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8161 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"COC8162 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8163 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8164 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8165 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8166 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8167 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8168 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {"hex":"3c7064","squawk":"2540","flight":"GEC8169 ","lat":56.139857,"lon":-3.054543,"nucp":7,"seen_pos":0.7,"altitude":32025,"vert_rate":64,"track":306,"speed":505,"category":"A5","mlat":[],"tisb":[],"messages":335,"seen":0.0,"rssi":-14.4},
+    {},
+    {"flight":"Poopy "}
+  ]
+}
---
+++ pi24-visualiser.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import liquidcrystal_i2c as i2c
+import time
+import json
+from tendo import singleton
+singleton.SingleInstance()
+
+
+def make_data_string(craft):
+	out = ""
+
+	if "flight" in craft.keys():
+		out += craft["flight"]
+	if "altitude" in craft.keys():
+		out += str(craft["altitude"]) + "ft "
+	if "speed" in craft.keys():
+		out += str(craft["speed"]) + "kn "
+
+	return out
+JSONFILE_FP = "/var/run/dump1090-mutability/aircraft.json"
+#JSONFILE_FP = "aircraft.json" # TESTING DATA
+DISPLAY = i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
+while True:
+	DISPLAY.clear()
+
+	JSONFILE = open(JSONFILE_FP, 'r')
+	data = json.load(JSONFILE)
+	JSONFILE.close()
+
+	aircraft = data["aircraft"]
+	if not aircraft:
+		DISPLAY.printline(0, "No Aircraft.")
+	j=0
+	for i in range(len(aircraft)):
+		print(i, j)
+		craft = aircraft[i]
+		if i % 4 == 0 and i > 0:
+			print "PEnis"
+			j = 0
+			time.sleep(5)
+			DISPLAY.clear()
+		if not "flight" in craft.keys():
+			DISPLAY.printline(j, "Unkown Aircraft.")
+			j += 1
+			continue
+		DISPLAY.printline(j, make_data_string(craft)[:20])
+		j += 1
+
+	time.sleep(5)
