f2d15933d37100ef2daa1105e6571a92242363ba
ACBob <acbobcat@protonmail.com>
2020-12-13 21:32:15 +0000

    Major refactors

--- pi24-visualiser.py
+++ pi24-visualiser.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
-import liquidcrystal_i2c as i2c
+# Some globals
+DEBUG = False
+
+try:
+	import liquidcrystal_i2c as i2c
+except ImportError:
+	print("pythonliquidcrystal_i2c Missing, Assuming a debug environment with no hardware!")
+	DEBUG = True
+
 import time
 import json
+import argparse
 
 
 def make_data_string(craft):
@@ -17,23 +26,83 @@ def make_data_string(craft):
 
 	return out
 
-def display(data: dict, DISPLAY):
-	DISPLAY.clear()
+def clear_display(disp):
+	if not DEBUG:
+		disp.clear()
+
+def print_to_display(disp, line, string):
+	if not DEBUG:
+		# TODO: hard-coded 20 character limit, there must be some way to ask the library what the screen allows.
+		#		That, or we can just add a command-line argument.
+		disp.printline(line, string[:20])
+	else:
+		print(string)
+
+def display(data, disp, sleeptime=5):
+	clear_display(disp)
+
 	aircraft = data["aircraft"]
+
 	if not aircraft:
-		DISPLAY.printline(0, "No Aircraft.")
-	j=0
+		print_to_display(disp, 0, "No Aircraft.")
+
+	j = 0
 	for i in range(len(aircraft)):
 		craft = aircraft[i]
-		if i % 4 == 0 and i > 0:
+
+		if i % 4 == 0 and i > 0: # TODO: Hard-coded 4 per-page limit, there must be some way to ask the library what the screen allows.
+								 #       Could also just make it an argument.
 			j = 0
-			time.sleep(5)
-			DISPLAY.clear()
+			time.sleep(sleeptime)
+			clear_display(disp)
+
 		if not "flight" in craft.keys():
-			DISPLAY.printline(j, "Unkown Aircraft.")
+			print_to_display(disp, j, "Unkown Aircraft.")
 			j += 1
 			continue
-		DISPLAY.printline(j, make_data_string(craft)[:20])
+
+		print_to_display(disp, j, make_data_string(craft))
+
 		j += 1
 
-	time.sleep(5)
+
+if __name__ == "__main__":
+	args = argparse.ArgumentParser(description="Tools to display data from FlightRadar24's Pi24 on LCD displays over GPIO.")
+	
+	args.add_argument("-t", "--test", type=bool,
+						help="Enables test mode, Forcing it to use stdout rather than actual hardware.")
+	args.add_argument("-d", "--data", type=str, required=True,
+						help="The file path of the data to use.")
+	args.add_argument("-w", "--wait", type=int, default=5,
+						help="The time to wait between each 'page' of text.")
+	args.add_argument("-l", "--loop", type=bool, default=False,
+						help="Enables looping, re-opening the file and displaying updated results.")
+
+	args = args.parse_args()
+
+	if args.test:
+		DEBUG = True
+	
+	json_file = open(args.data, 'r')
+	data = json.load(json_file)
+	json_file.close()
+
+	if not DEBUG:
+		# TODO: What is 0x27?
+		disp = i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
+	else:
+		disp = None
+	
+	# Python doesn't have Do ... While, so this is a kind-of hack to get the same outcome.
+	while True: # DO
+		display(data, disp, args.wait)
+
+		if not args.loop: # WHILE
+			break
+
+		time.sleep(args.wait)
+		
+		json_file = open(args.data, 'r')
+		data = json.load(json_file)
+		json_file.close()
+
