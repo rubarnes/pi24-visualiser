eda584662e7ee7bbe01d3e1b102681a72a34e29b
ACBob <acbobcat@protonmail.com>
2020-12-13 20:35:03 +0000

    Refactoring

--- pi24-visualiser.py
+++ pi24-visualiser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import liquidcrystal_i2c as i2c
 import time
@@ -16,22 +16,14 @@ def make_data_string(craft):
 		out += str(craft["speed"]) + "kn "
 
 	return out
-JSONFILE_FP = "/var/run/dump1090-mutability/aircraft.json"
-#JSONFILE_FP = "aircraft.json" # TESTING DATA
-DISPLAY = i2c.LiquidCrystal_I2C(0x27, 1, numlines=4)
-while True:
+
+def display(data: dict, DISPLAY):
 	DISPLAY.clear()
-
-	JSONFILE = open(JSONFILE_FP, 'r')
-	data = json.load(JSONFILE)
-	JSONFILE.close()
-
 	aircraft = data["aircraft"]
 	if not aircraft:
 		DISPLAY.printline(0, "No Aircraft.")
 	j=0
 	for i in range(len(aircraft)):
-		print(i, j)
 		craft = aircraft[i]
 		if i % 4 == 0 and i > 0:
 			j = 0
